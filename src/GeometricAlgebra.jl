36e845ea898cbc8a3cf56350acc02ea4da799a0b
Kevin Chu <kevin@velexi.com>
2020-09-11 12:33:15 -0700

    Remove zero() and one() functions.

--- src/GeometricAlgebra.jl
+++ src/GeometricAlgebra.jl
@@ -24,36 +24,6 @@ include("blade.jl")
 # --- Functions
 
 # Exports
-export zero, one
 
-
-"""
-    zero(B::AbstractBlade{T}) where {T<:AbstractFloat}
-    zero(::Type{S}) where {T<:AbstractFloat, S<:AbstractBlade{T}}
-    zero(::Type{Blade})
-    zero(::Type{Scalar})
-
-Return Zero (the additive identity). When the precision is not explicitly
-specified, it defaults to Float64.
-"""
-zero(B::AbstractBlade{T}) where {T<:AbstractFloat} = Zero{T}()
-zero(::Type{S}) where {T<:AbstractFloat, S<:AbstractBlade{T}} = Zero{T}()
-zero(::Type{Blade}) = Zero{Float64}()
-zero(::Type{Scalar}) = Zero{Float64}()
-
-
-"""
-    one(B::AbstractBlade{T}) where {T<:AbstractFloat}
-    one(::Type{S}) where {T<:AbstractFloat, S<:AbstractBlade{T}}
-    one(::Type{Blade})
-    one(::Type{Scalar})
-
-Return One (the multiplicative identity). When the precision is not explicitly
-specified, it defaults to Float64.
-"""
-one(B::AbstractBlade{T}) where {T<:AbstractFloat} = One{T}()
-one(::Type{S}) where {T<:AbstractFloat, S<:AbstractBlade{T}} = One{T}()
-one(::Type{Blade}) = One{Float64}()
-one(::Type{Scalar}) = One{Float64}()
 
 end  # End of GeometricAlgebra.jl module
