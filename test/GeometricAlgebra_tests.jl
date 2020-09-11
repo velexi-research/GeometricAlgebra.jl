b729525b2600994a41b9949d23b0aac30826cc7e
Kevin Chu <kevin@velexi.com>
2020-09-10 17:29:29 -0700

    Add tests for functions defined in GeometricAlgebra.jl

---
+++ test/GeometricAlgebra_tests.jl
@@ -0,0 +1,28 @@
+"""
+Unit tests for the GeometricAlgebra module.
+
+------------------------------------------------------------------------------
+COPYRIGHT/LICENSE. This file is part of the XYZ package. It is subject to
+the license terms in the LICENSE file found in the top-level directory of
+this distribution. No part of the XYZ package, including this file, may be
+copied, modified, propagated, or distributed except according to the terms
+contained in the LICENSE file.
+------------------------------------------------------------------------------
+"""
+# --- Imports
+
+# Standard library
+using Test
+
+# GeometricAlgebra.jl
+import GeometricAlgebra.Blade
+import GeometricAlgebra.Zero
+import GeometricAlgebra.zero
+
+
+# --- Function tests
+
+@testset "zero() tests" begin
+    B = Blade([1 2 3])
+    @test zero(B) === Zero
+end
