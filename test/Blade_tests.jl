9b406c2d7a9f6a582bc951f82949e0e4263fcfce
Kevin Chu <kevin@velexi.com>
2020-09-12 18:55:32 -0700

    Update unit tests for Blade type.

--- test/Blade_tests.jl
+++ test/Blade_tests.jl
@@ -12,8 +12,9 @@ except according to the terms contained in the LICENSE file.
 # --- Imports
 
 # Standard library
-using Test
+import InteractiveUtils.subtypes
 import LinearAlgebra
+using Test
 
 # GeometricAlgebra.jl
 using GeometricAlgebra
@@ -21,157 +22,224 @@ using GeometricAlgebra
 
 # --- Constructor tests
 
-@testset "Blade constructor tests: typeof(vectors) = Array{Float64}" begin
-    # multiple column vectors. number of vectors <= dimension of column space
-    vectors = [3. 3.; 4. 4; 0. 1.]
-    B = Blade(vectors)
-    @test B.dim == 3
-    @test B.grade == 2
-    @test size(B.basis) == (3, 2)
-    for i in size(B.basis, 2)
-        @test LinearAlgebra.norm(B.basis[:, i]) ≈ 1
+@testset "Blade: inner constructor tests" begin
+    # Notes
+    # -----
+    # * Test value of constructed instance
+
+    # --- Blade{T}(vectors::Matrix{T};
+    #              atol::Real=blade_zero(T)) where {T<:AbstractFloat}
+
+    for precision_type in subtypes(AbstractFloat)
+        # number of vectors <= dimension of column space
+        vectors = Matrix{precision_type}([3 -3; 4 -4; 0 1])
+        B = Blade{precision_type}(vectors)
+        @test B.dim == 3
+        @test B.grade == 2
+        @test size(B.basis) == (3, 2)
+        for i in size(B.basis, 2)
+            @test LinearAlgebra.norm(B.basis[:, i]) ≈ 1
+        end
+        @test B.norm ≈ 5
+
+        # number of vectors > dimension of column space
+        vectors = Matrix{precision_type}([1 2 3; 4 5 6])
+        B = Blade{precision_type}(vectors)
+        @test B === Zero{precision_type}()
+
+        # vectors are linearly dependent
+        vectors = Matrix{precision_type}([1 2 1; 1 2 4; 1 2 9])
+        B = Blade{precision_type}(vectors)
+        @test B === Zero{precision_type}()
+
+        # norm(blade) < atol
+        vectors = Matrix{precision_type}([3 3; 4 4; 0 1])
+        B = Blade{precision_type}(vectors, atol=6)
+        @test B === Zero{precision_type}()
     end
-    @test B.norm ≈ 5
 
-    # multiple column vectors. number of vectors > dimension of column space
-    vectors = [1. 2. 3.; 4. 5. 6.]
-    B = Blade(vectors)
-    @test B === Zero()
+    # --- Blade{T}(vector::Vector{T};
+    #              atol::Real=blade_zero(T)) where {T<:AbstractFloat}
 
-    # single column vector
-    vectors = [3.; 4.; 12.]
-    B = Blade(vectors)
-    @test B.dim == 3
-    @test B.grade == 1
-    @test B.basis ≈ Array{Float64}(vectors / 13)
-    @test LinearAlgebra.norm(B.basis) ≈ 1
-    @test B.norm ≈ 13
+    for precision_type in subtypes(AbstractFloat)
+        # Preparations
+        vector = [3, 4, 12]
 
-    # single row vector
-    vectors = [3. 4. 12.]
-    B = Blade(vectors)
-    @test B.dim == 3
-    @test B.grade == 1
-    @test B.basis ≈ Array{Float64}(permutedims(vectors / 13))
-    @test LinearAlgebra.norm(B.basis) ≈ 1
-    @test B.norm ≈ 13
+        # vector is a column vector
+        col_vector = Vector{precision_type}(vector)
+        B = Blade{precision_type}(col_vector)
+        @test B.dim == 3
+        @test B.grade == 1
+        @test B.basis ≈ reshape(col_vector / 13, length(vector), 1)
+        @test LinearAlgebra.norm(B.basis) ≈ 1
+        @test B.norm ≈ 13
 
-    # multiple column vectors with norm less than atol
-    vectors = [3. 3.; 4. 4; 0. 1.]
-    B = Blade(vectors, atol=6)
-    @test B === Zero()
+        # vector is a row vector
+        row_vector = reshape(Array{precision_type}(vector), 1, length(vector))
+        B = Blade{precision_type}(row_vector)
+        @test B.dim == 3
+        @test B.grade == 1
+        @test B.basis ≈ permutedims(row_vector / 13)
+        @test LinearAlgebra.norm(B.basis) ≈ 1
+        @test B.norm ≈ 13
 
-    # vectors are linearly dependent ==> blade is zero
-    vectors = [1. 2. 1.; 1. 2. 4; 1. 2. 9]
-    B = Blade(vectors)
-    @test B === Zero()
+        # vector is a zero vector
+        zero_vector = Array{precision_type}([0. 0. 0.])
+        B = Blade(zero_vector)
+        @test B === Zero{precision_type}()
 
-    # vectors is a single zero vector
-    vectors = [0. 0. 0.]
-    B = Blade(vectors)
-    @test B === Zero()
+        # norm(vector) < default atol
+        small_vector = Vector{precision_type}(vector)
+        small_vector /= LinearAlgebra.norm(small_vector)
+        small_vector *= blade_atol(precision_type) / 2
+        B = Blade{precision_type}(small_vector)
+        @test B === Zero{precision_type}()
+
+        # norm(vector) < atol
+        col_vector = Vector{precision_type}(vector)
+        B = Blade{precision_type}(col_vector,
+                                  atol=LinearAlgebra.norm(col_vector) + 1)
+        @test B === Zero{precision_type}()
+    end
 end
 
-@testset "Blade constructor tests: typeof(vectors) = Array{AbstractFloat}" begin
-    # multiple column vectors. number of vectors <= dimension of column space
-    vectors = Array{Float32}([3. 3.; 4. 4.; 0. 1.])
-    B = Blade(vectors)
-    @test B.dim == 3
-    @test B.grade == 2
-    @test size(B.basis) == (3, 2)
-    for i in size(B.basis, 2)
-        @test LinearAlgebra.norm(B.basis[:, i]) ≈ 1
+@testset "Blade: outer constructor tests" begin
+    # Notes
+    # -----
+    # * Test type of constructed instances. Correct construction of instances
+    #   is tested by the inner constructor tests.
+    #
+    # * Test behavior of `atol` argument.
+
+    # --- Preparations
+
+    vectors = [3 3; -4 -4; 0 1]
+
+    # --- Blade(vectors::Array{T};
+    #           atol::Real=blade_atol(T)) where {T<:AbstractFloat}
+
+    for precision_type in subtypes(AbstractFloat)
+        # Preparations
+        converted_vectors = Matrix{precision_type}(vectors)
+
+        # norm(blade) > default atol
+        B = Blade(converted_vectors)
+        @test B isa Blade{precision_type}
+
+        # norm(blade) < default atol
+        small_blade = blade_atol(precision_type) * converted_vectors / 6
+        B = Blade(small_blade)
+        @test B === Zero{precision_type}()
+
+        # norm(blade) < atol
+        B = Blade(converted_vectors, atol=6)
+        @test B === Zero{precision_type}()
     end
-    @test B.norm ≈ 5
 
-    # multiple column vectors. number of vectors > dimension of column space
-    vectors = Array{Float16}([1. 2. 3.; 4. 5. 6.])
-    B = Blade(vectors)
-    @test B === Zero()
+    # --- Blade{T}(vectors::Array{S};
+    #              atol::Real=blade_atol(T)) where {T<:AbstractFloat,
+    #                                               S<:AbstractFloat}
 
-    # single column vector
-    vectors = Array{Float32}([3.; 4.; 12.])
-    B = Blade(vectors)
-    @test B.dim == 3
-    @test B.grade == 1
-    @test B.basis ≈ Array{Float32}(vectors / 13)
-    @test LinearAlgebra.norm(B.basis) ≈ 1
-    @test B.norm ≈ 13
+    for precision_type in subtypes(AbstractFloat)
+        for value_type in subtypes(AbstractFloat)
+            # Preparations
+            converted_vectors = Matrix{value_type}(vectors)
 
-    # single row vector
-    vectors = Array{Float16}([3. 4. 12.])
-    B = Blade(vectors)
-    @test B.dim == 3
-    @test B.grade == 1
-    @test B.basis ≈ Array{Float16}(permutedims(vectors / 13))
-    @test LinearAlgebra.norm(B.basis) ≈ 1
-    @test B.norm ≈ 13
+            # norm(blade) > default atol
+            B = Blade{precision_type}(converted_vectors)
+            @test B isa Blade{precision_type}
 
-    # multiple column vectors with norm less than atol
-    vectors = Array{Float32}([3. 3.; 4. 4; 0. 1.])
-    B = Blade(vectors, atol=6)
-    @test B === Zero()
+            # norm(blade) < default atol
+            small_blade = blade_atol(precision_type) * converted_vectors / 6
+            B = Blade{precision_type}(small_blade)
+            @test B === Zero{precision_type}()
 
-    # vectors are linearly dependent ==> blade is zero
-    vectors = Array{Float16}([1. 2. 1.; 1. 2. 4; 1. 2. 9])
-    B = Blade(vectors)
-    @test B === Zero()
-
-    # vectors is a single zero vector
-    vectors = Vector{Float32}([0.; 0.; 0.])
-    B = Blade(vectors)
-    @test B === Zero()
-end
-
-@testset "Blade constructor tests: typeof(vectors) = Array{Integer}" begin
-    # multiple column vectors. number of vectors <= dimension of column space
-    vectors = Array{Int64}([3 3; 4 4; 0 1])
-    B = Blade(vectors)
-    @test B.dim == 3
-    @test B.grade == 2
-    @test size(B.basis) == (3, 2)
-    for i in size(B.basis, 2)
-        @test LinearAlgebra.norm(B.basis[:, i]) ≈ 1
+            # norm(blade) < atol
+            B = Blade{precision_type}(converted_vectors, atol=6)
+            @test B === Zero{precision_type}()
+        end
     end
-    @test B.norm ≈ 5
 
-    # multiple column vectors. number of vectors > dimension of column space
-    vectors = Array{Int32}([1 2 3; 4 5 6])
-    B = Blade(vectors)
-    @test B === Zero()
+    # --- Blade(vectors::Array{Integer}; atol::Real=blade_atol(Float64))
 
-    # single column vector
-    vectors = Array{Int16}([3; 4; 12])
-    B = Blade{Float32}(vectors)
-    @test B.dim == 3
-    @test B.grade == 1
-    @test B.basis ≈ Array{Float32}(vectors / 13)
-    @test LinearAlgebra.norm(B.basis) ≈ 1
-    @test B.norm ≈ 13
+    # subtypes(Signed)
+    for value_type in subtypes(Signed)
+        # Preparations
+        converted_vectors = Matrix{value_type}(vectors)
 
-    # single row vector
-    vectors = Vector{Int64}([3; 4; 12])
-    B = Blade{Float16}(vectors)
-    @test B.dim == 3
-    @test B.grade == 1
-    @test B.basis ≈ Array{Float16}(vectors / 13)
-    @test LinearAlgebra.norm(B.basis) ≈ 1
-    @test B.norm ≈ 13
+        # norm(blade) > default atol
+        B = Blade(converted_vectors)
+        @test B isa Blade{Float64}
 
-    # multiple column vectors with norm less than atol
-    vectors = Array{Int32}([3 3; 4 4; 0 1])
-    B = Blade{Float32}(vectors, atol=6)
-    @test B === Zero()
+        # norm(blade) < atol
+        B = Blade(converted_vectors, atol=6)
+        @test B === Zero{Float64}()
+    end
 
-    # vectors are linearly dependent ==> blade is zero
-    vectors = Array{Int16}([1 2 1; 1 2 4; 1 2 9])
-    B = Blade{Float16}(vectors)
-    @test B === Zero()
+    # subtypes(Unsigned)
+    for value_type in subtypes(Unsigned)
+        # Preparations
+        converted_vectors = Matrix{value_type}(abs.(vectors))
 
-    # vectors is a single zero vector
-    vectors = Vector{Int64}([0; 0; 0])
-    B = Blade{Float32}(vectors)
-    @test B === Zero
+        # norm(blade) > default atol
+        B = Blade(converted_vectors)
+        @test B isa Blade{Float64}
+
+        # norm(blade) < atol
+        B = Blade(converted_vectors, atol=6)
+        @test B === Zero{Float64}()
+    end
+
+    # Bool: norm(blade) < atol
+    converted_vectors = Matrix{Bool}(vectors .!= 0)
+    B = Blade(converted_vectors)
+    @test B isa Blade{Float64}
+
+    # Bool: norm(blade) < atol
+    B = Blade(converted_vectors, atol=2)
+    @test B === Zero{Float64}()
+
+    # --- Blade{T}(vectors::Array{Integer};
+    #              atol::Real=blade_atol(T)) where {T<:AbstractFloat}
+
+    for precision_type in subtypes(AbstractFloat)
+        # subtypes(Signed)
+        for value_type in subtypes(Signed)
+            # Preparations
+            converted_vectors = Matrix{value_type}(vectors)
+
+            # norm(blade) > default atol
+            B = Blade{precision_type}(converted_vectors)
+            @test B isa Blade{precision_type}
+
+            # norm(blade) < atol
+            B = Blade{precision_type}(converted_vectors, atol=6)
+            @test B === Zero{precision_type}()
+        end
+
+        # subtypes(Unsigned)
+        for value_type in subtypes(Unsigned)
+            # Preparations
+            converted_vectors = Matrix{value_type}(abs.(vectors))
+
+            # norm(blade) > default atol
+            B = Blade{precision_type}(converted_vectors)
+            @test B isa Blade{precision_type}
+
+            # norm(blade) < atol
+            B = Blade{precision_type}(converted_vectors, atol=6)
+            @test B === Zero{precision_type}()
+        end
+
+        # Bool: norm(blade) < atol
+        converted_vectors = Matrix{Bool}(vectors .!= 0)
+        B = Blade{precision_type}(converted_vectors)
+        @test B isa Blade{precision_type}
+
+        # Bool: norm(blade) < atol
+        B = Blade{precision_type}(converted_vectors, atol=2)
+        @test B === Zero{precision_type}()
+    end
 end
 
 # --- Function tests
