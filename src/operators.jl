24488e7d182540cfb301a41079fe5fdbd6eecff8
Kevin Chu <kevin@velexi.com>
2020-09-15 09:19:58 -0700

    Replace access to data elements with function calls.
    * This approach emphasizes type behavior rather than object structure
      and leverages Julia's multiple-dispatch design.

--- src/operators.jl
+++ src/operators.jl
@@ -22,25 +22,14 @@ import Base.:(==), Base.:(≈), Base.:(-)
 Return true if B1 and B2 are equal; otherwise, return false.
 """
 ==(B1::Blade, B2::Blade) =  # TODO: fix basis and orientation comparison
-    B1.dim == B2.dim && B1.grade == B2.grade &&
-    B1.norm == B2.norm &&
-    B1.basis == B2.basis && B1.sign == B2.sign
+    dim(B1) == dim(B2) && grade(B1) == grade(B2) &&
+    value(B1) == value(B2) && basis(B1) == basis(B2)
 
-==(B1::Scalar, B2::Scalar) = (B1.value == B2.value)
+==(B1::AbstractScalar, B2::AbstractScalar) = (value(B1) == value(B2))
 
-==(B::Scalar, x::Real) = (x == B.value)
-==(x::Real, B::Scalar) = (B == x)
+==(B::AbstractScalar, x::Real) = (x == value(B))
+==(x::Real, B::AbstractScalar) = (B == x)
 
-==(B1::Scalar, B2::One) = (B1.value == 1)
-==(B1::One, B2::Scalar) = (B2 == B1)
-
-==(B1::Zero, B2::Zero) = true
-==(B::Zero, x::Real) = (x == 0)
-==(x::Real, B::Zero) = (B == x)
-
-==(B1::One, B2::One) = true
-==(B::One, x::Real) = (x == 1)
-==(x::Real, B::One) = (B == x)
 
 """
     ≈(B1::AbstractBlade{<:AbstractFloat}, B2::AbstractBlade{<:AbstractFloat})
@@ -51,19 +40,19 @@ Return true if B1 and B2 are approximatly equal; otherwise, return false.
   atol::Real=0,
   rtol::Real=atol>0 ? 0 : max(√eps(T1), √eps(T2))) where {T1<:AbstractFloat,
                                                           T2<:AbstractFloat} =
-    B1.sign == B2.sign &&
-    ≈(B1.norm, B2.norm, atol=atol, rtol=rtol) &&
+    sign(B1) == sign(B2) &&
+    ≈(norm(B1), norm(B2), atol=atol, rtol=rtol) &&
     true  # TODO: add check that basis represent the same space
 
 ≈(B1::Scalar{T1}, B2::Scalar{T2};
   atol::Real=0,
   rtol::Real=atol>0 ? 0 : max(√eps(T1), √eps(T2))) where {T1<:AbstractFloat,
                                                           T2<:AbstractFloat} =
-    ≈(B1.value, B2.value, atol=atol, rtol=rtol)
+    ≈(value(B1), value(B2), atol=atol, rtol=rtol)
 
 ≈(B::Scalar{T}, x::Real;
   atol::Real=0, rtol::Real=atol>0 ? 0 : sqrt(eps(T))) where {T<:AbstractFloat} =
-    ≈(x, B.value, rtol=rtol, atol=atol)
+    ≈(x, value(B), rtol=rtol, atol=atol)
 ≈(x::Real, B::Scalar{T};
   atol::Real=0, rtol::Real=atol>0 ? 0 : sqrt(eps(T))) where {T<:AbstractFloat} =
     ≈(B, x, rtol=rtol, atol=atol)
@@ -79,8 +68,8 @@ export negation, reciprocal
 
 Return the additive inverse of `B`.
 """
-negation(B::Blade{<:AbstractFloat}) = Blade(B, norm=norm(B), sign=-B.sign)
-negation(B::Scalar) = Scalar(-B.value)
+negation(B::Blade{<:AbstractFloat}) = Blade(B, norm=norm(B), sign=-sign(B))
+negation(B::Scalar) = Scalar(-value(B))
 
 -(B::AbstractBlade{<:AbstractFloat}) = negation(B)
 
@@ -91,10 +80,10 @@ Return the multiplicative inverse of `B`.
 """
 reciprocal(B::Blade{<:AbstractFloat}) =
     mod(grade(B), 4) < 2 ?
-        Blade(B, norm=1 / norm(B), sign=B.sign) :
-        Blade(B, norm=1 / norm(B), sign=-B.sign)
+        Blade(B, norm=1 / norm(B), sign=sign(B)) :
+        Blade(B, norm=1 / norm(B), sign=-sign(B))
 
-reciprocal(B::Scalar{<:AbstractFloat}) = Scalar(1 / B.value)
+reciprocal(B::Scalar{<:AbstractFloat}) = Scalar(1 / value(B))
 reciprocal(B::Zero{T}) where {T<:AbstractFloat} = Scalar{T}(Inf)
 reciprocal(B::One{T}) where {T<:AbstractFloat} = One{T}()
 
@@ -105,26 +94,23 @@ reciprocal(B::One{T}) where {T<:AbstractFloat} = One{T}()
 export ∧, outer
 
 """
-    ∧(B::Blade, C::Blade)
-    ∧(B::Blade, C::Vector)
-    ∧(B::Vector, C::Blade)
+    ∧(B::AbstractBlade, C::AbstractBlade)
+    ∧(B::AbstractBlade, C::Vector)
+    ∧(B::Vector, C::AbstractBlade)
     ∧(v::Vector, w::Vector)
 
-Return the outer product of a combination of vectors and blades.
+Return the outer product of a combination of blades and vectors.
 """
-function ∧(B::Blade{T}, C::Blade{T}) where {T<:AbstractFloat}
-    outer_product = Blade{T}(hcat(basis(B), basis(C)))
-    if outer_product == 0
-        return Zero(T)
-    else
-        return Blade{T}(outer_product,
-                        norm=B.norm * C.norm,
-                        sign=outer_product.sign * B.sign * C.sign)
-    end
-end
+∧(B::Blade, C::Blade) =
+    Blade(hcat(basis(B), basis(C)),
+          norm=norm(B) * norm(C), sign=sign(B) * sign(C))
+∧(B::Blade, C::AbstractScalar) =
+    Blade(B, norm=norm(B) * norm(C), sign=sign(B) * sign(C))
+∧(B::AbstractScalar, C::Blade) =
+    Blade(C, norm=norm(B) * norm(C), sign=sign(B) * sign(C))
 
-∧(B::Blade{T}, C::Vector{T}) where {T<:AbstractFloat} = B ∧ Blade(C)
-∧(B::Vector{T}, C::Blade{T}) where {T<:AbstractFloat} = Blade(B) ∧ C
+∧(B::Blade, v::Vector) = B ∧ Blade(v)
+∧(v::Vector, B::Blade) = Blade(v) ∧ B
 ∧(v::Vector{<:AbstractFloat}, w::Vector{<:AbstractFloat}) = Blade(v) ∧ Blade(w)
 const outer = ∧
 
