c70a61c03b057286dd2e9d93b5d2f3621bc767b7
Kevin Chu <kevin@velexi.com>
2020-10-14 17:34:08 -0700

    Add unit tests for core blade operators.

--- test/blade_core_operator_tests.jl
+++ test/blade_core_operator_tests.jl
@@ -23,13 +23,30 @@ using GeometricAlgebra
 @testset "*(B, C) tests: B or C isa {Scalar, Real}" begin
     # --- Preparations
 
+    # Test values
     test_value_1 = rand()
     test_value_1 = rand() > 0.5 ? test_value_1 : -test_value_1
 
     test_value_2 = rand()
     test_value_2 = rand() > 0.5 ? test_value_2 : -test_value_2
 
+    # Blade vectors
     vectors = randn(6, 3)
+
+    # Pseudoscalar dimension
+    dim = 100
+
+    # --- x::Real, B::Scalar
+    #     B::Scalar, x::Real
+
+    # Preparations
+    x = test_value_1
+    B = Scalar(test_value_2)
+
+    # Exercise functionality and check results
+    expected_x_times_B = Scalar(x * value(B))
+    @test x * B == expected_x_times_B
+    @test B * x == expected_x_times_B
 
     # --- B::Scalar, C::Scalar
 
@@ -65,13 +82,23 @@ using GeometricAlgebra
     @test B * C ≈ expected_B_times_C
     @test C * B ≈ expected_B_times_C
 
+    # --- x::Real, B::Pseudoscalar
+    #     B::Pseudoscalar, x::Real
+
+    # Preparations
+    x = test_value_1
+    B = Pseudoscalar(dim, test_value_2)
+
+    # Exercise functionality and check results
+    expected_x_times_B = Pseudoscalar(dim, x * value(B))
+    @test x * B == expected_x_times_B
+    @test B * x == expected_x_times_B
+
     # --- B::Scalar, C::Pseudoscalar
     #     B::Pseudoscalar, C::Scalar
 
     # Preparations
     B = Scalar(test_value_1)
-
-    dim = 3
     C = Pseudoscalar(dim, test_value_2)
 
     # Exercise functionality and check results
@@ -83,72 +110,90 @@ end
 # --- ∧(B, C)
 
 @testset "∧(B, C) tests: B or C isa {Scalar, Real}" begin
+    # --- Preparations
+
+    # Test values
+    test_value_1 = rand()
+    test_value_1 = rand() > 0.5 ? test_value_1 : -test_value_1
+
+    test_value_2 = rand()
+    test_value_2 = rand() > 0.5 ? test_value_2 : -test_value_2
+
+    # Blade vectors
+    vectors = randn(6, 3)
+
+    # Pseudoscalar dimension
+    dim = 100
+
+    # --- x::Real, B::Scalar
+    #     B::Scalar, x::Real
+
+    # Preparations
+    x = test_value_1
+    B = Scalar(test_value_2)
+
+    # Exercise functionality and check results
+    expected_x_wedge_B = Scalar(x * value(B))
+    @test x ∧ B == expected_x_wedge_B
+    @test B ∧ x == expected_x_wedge_B
+
     # --- B::Scalar, C::Scalar
 
     # Preparations
-    B_value = rand()
-    B_value = rand() > 0.5 ? B_value : -B_value
-    B = Scalar(B_value)
+    B = Scalar(test_value_1)
+    C = Scalar(test_value_2)
 
-    C_value = rand()
-    C_value = rand() > 0.5 ? C_value : -C_value
-    C = Scalar(C_value)
+    # Exercise functionality and check results
+    expected_B_wedge_C = Scalar(value(B) * value(C))
+    @test B ∧ C == expected_B_wedge_C
 
-    expected_B_wedge_C = Scalar(B_value * C_value)
-
-    # Exercise functionality
-    B_wedge_C = B ∧ C
-    @test B_wedge_C ≈ expected_B_wedge_C
-    @test B_wedge_C == outer(B, C)
-
-    # --- B::Blade, C::Scalar
-    #     B::Scalar, C::Blade
+    # --- x::Real, B::Blade
+    #     B::Blade, x::Real
 
     # Preparations
-    B_vectors = hcat([1; 0; 0; 0; 0],
-                     [0; 2; 0; 0; 0])
-    B = Blade(B_vectors)
+    x = test_value_1
+    B = Blade(vectors)
 
-    value = rand() + 1  # add 1 to avoid 0
-    value = rand() > 0.5 ? value : -value
-    C = Scalar(value)
+    # Exercise functionality and check results
+    expected_x_wedge_B = Blade(basis(B), volume=x * volume(B))
+    @test x ∧ B ≈ expected_x_wedge_B
+    @test B ∧ x ≈ expected_x_wedge_B
 
-    expected_B_wedge_C = Blade(B_vectors, volume=2 * value)
-
-    # Exercise functionality
-    B_wedge_C = B ∧ C
-    @test B_wedge_C ≈ expected_B_wedge_C
-    @test B_wedge_C == outer(B, C)
-
-    C_wedge_B = C ∧ B
-    @test C_wedge_B ≈ expected_B_wedge_C
-    @test C_wedge_B == outer(C, B)
-
-    # --- B::Blade, C::Real
-    #     B::Real, C::Blade
+    # --- B::Scalar, C::Blade
+    #     B::Blade, C::Scalar
 
     # Preparations
-    B_vectors = hcat([1; 0; 0; 0; 0],
-                     [0; 2; 0; 0; 0])
-    B = Blade(B_vectors)
+    B = Scalar(test_value_1)
+    C = Blade(vectors)
 
-    C = 2.5
+    # Exercise functionality and check results
+    expected_B_wedge_C = Blade(basis(C), volume=value(B) * volume(C))
+    @test B ∧ C ≈ expected_B_wedge_C
+    @test C ∧ B ≈ expected_B_wedge_C
 
-    expected_B_wedge_C = Blade(B_vectors, volume=5.0)
+    # --- x::Real, B::Pseudoscalar
+    #     B::Pseudoscalar, x::Real
 
-    # Exercise functionality
-    B_wedge_C = B ∧ C
-    @test B_wedge_C == expected_B_wedge_C
-    @test B_wedge_C == outer(B, C)
+    # Preparations
+    x = test_value_1
+    B = Pseudoscalar(dim, test_value_2)
 
-    C_wedge_B = C ∧ B
-    @test C_wedge_B == expected_B_wedge_C
-    @test C_wedge_B == outer(C, B)
+    # Exercise functionality and check results
+    expected_x_wedge_B = Pseudoscalar(dim, x * value(B))
+    @test x ∧ B == expected_x_wedge_B
+    @test B ∧ x == expected_x_wedge_B
 
     # --- B::Scalar, C::Pseudoscalar
     #     B::Pseudoscalar, C::Scalar
 
-    # TODO
+    # Preparations
+    B = Scalar(test_value_1)
+    C = Pseudoscalar(dim, test_value_2)
+
+    # Exercise functionality and check results
+    expected_B_wedge_C = Pseudoscalar(dim, value(B) * value(C))
+    @test B ∧ C ≈ expected_B_wedge_C
+    @test C ∧ B ≈ expected_B_wedge_C
 end
 
 @testset "∧(B, C) tests: B, C::{Blade, Vector}" begin
