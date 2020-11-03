f77f1573d0eac65ed5dd6196773b1b631f4bc34f
Kevin Chu <kevin@velexi.com>
2020-11-02 21:55:28 -0800

    Move comparison operation tests for Blade type.

--- test/Blade_tests.jl
+++ test/Blade_tests.jl
@@ -979,6 +979,142 @@ end
     end
 end
 
+# --- Test comparison operations
+
+@testset "Blade: ==(B, C)" begin
+    # Preparations
+    vectors = [3 3; 4 4; 0 1]
+    B = Blade(vectors)
+
+    # dim(B) == dim(C), grade(B) == grade(C), volume(B) == volume(C)
+    # basis(B) == basis(C)
+    for precision_type1 in subtypes(AbstractFloat)
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Blade(convert(Array{precision_type1}, vectors))
+            C = Blade(convert(Array{precision_type2}, vectors))
+            if precision_type1 == precision_type2
+                @test B == C
+            else
+                @test B != C
+            end
+        end
+    end
+
+    # dim(B) != dim(C)
+    for precision_type1 in subtypes(AbstractFloat)
+        vectors2 = [3 3; 4 4; 0 1; 0 1]
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Blade(convert(Array{precision_type1}, vectors))
+            C = Blade(convert(Array{precision_type2}, vectors2))
+            @test B != C
+        end
+    end
+
+    # grade(B) != grade(C)
+    for precision_type1 in subtypes(AbstractFloat)
+        vectors2 = [3 3 3; 4 4 4; 0 1 0; 0 0 1]
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Blade(convert(Array{precision_type1}, vectors))
+            C = Blade(convert(Array{precision_type2}, vectors2))
+            @test B != C
+        end
+    end
+
+    # volume(B) != volume(C)
+    for precision_type1 in subtypes(AbstractFloat)
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Blade(convert(Array{precision_type1}, vectors))
+            C = Blade(convert(Array{precision_type2}, vectors),
+                      volume=2*volume(B))
+            @test B != C
+        end
+    end
+
+    # basis(B) != basis(C)
+    test_volume = 5.0
+    for precision_type1 in subtypes(AbstractFloat)
+        vectors2 = [3 4; 4 5; 0 1]
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Blade(convert(Array{precision_type1}, vectors),
+                      volume=test_volume)
+            C = Blade(convert(Array{precision_type2}, vectors2),
+                      volume=test_volume)
+            @test B != C
+        end
+    end
+end
+
+@testset "Blade: ≈(B, C)" begin
+    # Preparations
+    vectors = [3 3; 4 4; 0 1]
+    B = Blade(vectors)
+
+    # dim(B) == dim(C), grade(B) == grade(C), volume(B) ≈ volume(C)
+    # basis(B) ≈ basis(C)
+    for precision_type1 in subtypes(AbstractFloat)
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Blade(convert(Array{precision_type1}, vectors))
+            C = Blade(convert(Array{precision_type2}, vectors))
+            @test B ≈ C
+        end
+    end
+
+    # dim(B) != dim(C)
+    for precision_type1 in subtypes(AbstractFloat)
+        vectors2 = [3 3; 4 4; 0 1; 0 1]
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Blade(convert(Array{precision_type1}, vectors))
+            C = Blade(convert(Array{precision_type2}, vectors2))
+            @test B ≉ C
+        end
+    end
+
+    # grade(B) != grade(C)
+    for precision_type1 in subtypes(AbstractFloat)
+        vectors2 = [3 3 3; 4 4 4; 0 1 0; 0 0 1]
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Blade(convert(Array{precision_type1}, vectors))
+            C = Blade(convert(Array{precision_type2}, vectors2))
+            @test B ≉ C
+        end
+    end
+
+    # volume(B) ≉ volume(C)
+    for precision_type1 in subtypes(AbstractFloat)
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Blade(convert(Array{precision_type1}, vectors))
+            C = Blade(convert(Array{precision_type2}, vectors),
+                      volume=2*volume(B))
+            @test B ≉ C
+        end
+    end
+
+    # basis(B) ≉ basis(C)
+    test_volume = 5.0
+    for precision_type1 in subtypes(AbstractFloat)
+        vectors2 = [3 4; 4 10; 0 1]
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Blade(convert(Array{precision_type1}, vectors),
+                      volume=test_volume)
+            C = Blade(convert(Array{precision_type2}, vectors2),
+                      volume=test_volume)
+            @test B ≉ C
+        end
+    end
+
+    # B and C have opposite orientations
+    test_volume = 5.0
+    for precision_type1 in subtypes(AbstractFloat)
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Blade(convert(Array{precision_type1}, vectors),
+                      volume=test_volume)
+            C = Blade(convert(Array{precision_type2}, vectors),
+                      volume=-test_volume)
+            @test B ≉ C
+        end
+    end
+end
+
 # --- Tests for AbstractMultivector interface functions
 
 @testset "Blade: -(B)" begin
