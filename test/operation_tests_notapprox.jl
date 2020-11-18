da2b10d1ba88bd00b0c7a471a161f46aea9db3dd
Kevin Chu <kevin@velexi.com>
2020-11-17 16:40:35 -0800

    Add placeholders for comparison operator tests.

--- test/operation_tests_notapprox.jl
+++ test/operation_tests_notapprox.jl
@@ -20,54 +20,326 @@ using GeometricAlgebra
 
 # --- Tests
 
-@testset "!isapprox(M, N)" begin
-    # Preparations
+# ------ M::Multivector
+
+@testset "!isapprox(M::Multivector, N::Blade)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(M::Multivector, N::Pseudoscalar)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(M::Multivector, N::Scalar)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(M::Multivector, N::One)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(M::Multivector, N::Zero)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(M::Multivector, N::Real)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(M::Multivector, N::Vector)" begin
+    @test_skip 1
+end
+
+# ------ B::Blade
+
+@testset "!isapprox(B::Blade, M::Multivector)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Blade, C::Pseudoscalar)" begin
     vectors = [3 3; 4 4; 0 1]
     test_dim = size(vectors, 1)
-    test_value = rand()
-    test_value = rand() > 0.5 ? test_value : -test_value
-
-    # --- B::AbstractBlade, C::AbstractBlade
+    test_value = 5
 
     for precision_type1 in subtypes(AbstractFloat)
         for precision_type2 in subtypes(AbstractFloat)
-            # B::Blade, C::Scalar
-            # B::Scalar, C::Blade
+            B = Blade{precision_type1}(vectors)
+            C = Pseudoscalar{precision_type2}(test_dim, test_value)
+            @test B ≉ C
+        end
+    end
+end
+
+@testset "!isapprox(B::Blade, C::Scalar)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
+
+    for precision_type1 in subtypes(AbstractFloat)
+        for precision_type2 in subtypes(AbstractFloat)
             B = Blade{precision_type1}(vectors)
             C = Scalar{precision_type2}(test_value)
             @test B ≉ C
-            @test C ≉ B
+        end
+    end
+end
 
-            # B::Blade, C::Pseudoscalar
-            # B::Pseudoscalar, C::Blade
-            B = Blade{precision_type1}(vectors)
-            C = Pseudoscalar{precision_type2}(test_dim, test_value)
+@testset "!isapprox(B::Blade, C::One)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Blade, C::Zero)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Blade, C::Real)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
+
+    for precision_type in subtypes(AbstractFloat)
+        B = Blade{precision_type}(vectors)
+        C = test_value
+        @test B ≉ C
+    end
+end
+
+@testset "!isapprox(B::Blade, C::Vector)" begin
+    @test_skip 1
+end
+
+# ------ B::Pseudoscalar
+
+@testset "!isapprox(B::Pseudoscalar, M::Multivector)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Pseudoscalar, C::Blade)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
+
+    for precision_type1 in subtypes(AbstractFloat)
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Pseudoscalar{precision_type2}(test_dim, test_value)
+            C = Blade{precision_type1}(vectors)
             @test B ≉ C
-            @test C ≉ B
+        end
+    end
+end
 
-            # B::Scalar, C::Pseudoscalar
-            # B::Pseudoscalar, C::Scalar
+@testset "!isapprox(B::Pseudoscalar, C::Scalar)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
+
+    for precision_type1 in subtypes(AbstractFloat)
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Pseudoscalar{precision_type2}(test_dim, test_value)
+            C = Scalar{precision_type1}(test_value)
+            @test B ≉ C
+        end
+    end
+end
+
+@testset "!isapprox(B::Pseudoscalar, C::One)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Pseudoscalar, C::Zero)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Pseudoscalar, C::Real)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
+
+    for precision_type in subtypes(AbstractFloat)
+        B = Pseudoscalar{precision_type}(test_dim, test_value)
+        C = test_value
+        @test B ≉ C
+    end
+end
+
+@testset "!isapprox(B::Pseudoscalar, C::Vector)" begin
+    @test_skip 1
+end
+
+# ------ B::Scalar
+
+@testset "!isapprox(B::Scalar, M::Multivector)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Scalar, C::Blade)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
+
+    for precision_type1 in subtypes(AbstractFloat)
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Scalar{precision_type2}(test_value)
+            C = Blade{precision_type1}(vectors)
+            @test B ≉ C
+        end
+    end
+end
+
+@testset "!isapprox(B::Scalar, C::Pseudoscalar)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
+
+    for precision_type1 in subtypes(AbstractFloat)
+        for precision_type2 in subtypes(AbstractFloat)
             B = Scalar{precision_type1}(test_value)
             C = Pseudoscalar{precision_type2}(test_dim, test_value)
             @test B ≉ C
-            @test C ≉ B
         end
     end
+end
 
-    # --- B::Union{Blade, Pseudoscalar}, C::Real
-    #     B::Real, C::Union{Blade, Pseudoscalar}
+@testset "!isapprox(B::Scalar, C::One)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Scalar, C::Zero)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Scalar, C::Real)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Scalar, C::Vector)" begin
+    @test_skip 1
+end
+
+# ------ B::One
+
+@testset "!isapprox(B::One, M::Multivector)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::One, C::Blade)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::One, C::Pseudoscalar)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::One, C::Scalar)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::One, C::Zero)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::One, C::Real)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::One, C::Vector)" begin
+    @test_skip 1
+end
+
+# ------ B::Zero
+
+@testset "!isapprox(B::Zero, M::Multivector)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Zero, C::Blade)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Zero, C::Pseudoscalar)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Zero, C::Scalar)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Zero, C::One)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Zero, C::Real)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Zero, C::Vector)" begin
+    @test_skip 1
+end
+
+# ------ B::Real
+
+@testset "!isapprox(B::Real, M::Multivector)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Real, C::Blade)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
 
     for precision_type in subtypes(AbstractFloat)
-        # B::Blade, C::Real
-        # B::Real, C::Blade
-        B = Blade{precision_type}(vectors)
-        @test B ≉ test_value
-        @test test_value ≉ B
-
-        # B::Pseudoscalar, C::Real
-        # B::Real, C::Pseudoscalar
-        B = Pseudoscalar{precision_type}(test_dim, test_value)
-        @test B ≉ test_value
-        @test test_value ≉ B
+        B = test_value
+        C = Blade{precision_type}(vectors)
+        @test B ≉ C
     end
+end
+
+@testset "!isapprox(B::Real, C::Pseudoscalar)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
+
+    for precision_type in subtypes(AbstractFloat)
+        B = test_value
+        C = Pseudoscalar{precision_type}(test_dim, test_value)
+        @test B ≉ C
+    end
+end
+
+@testset "!isapprox(B::Real, C::Scalar)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Real, C::One)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Real, C::Zero)" begin
+    @test_skip 1
+end
+
+# ------ B::Vector
+
+@testset "!isapprox(B::Vector, M::Multivector)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Vector, C::Blade)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Vector, C::Pseudoscalar)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Vector, C::Scalar)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Vector, C::One)" begin
+    @test_skip 1
+end
+
+@testset "!isapprox(B::Vector, C::Zero)" begin
+    @test_skip 1
 end
--- test/operation_tests_notequal.jl
+++ test/operation_tests_notequal.jl
@@ -20,54 +20,326 @@ using GeometricAlgebra
 
 # --- Tests
 
-@testset "!=(M, N)" begin
-    # Preparations
+# ------ M::Multivector
+
+@testset "!=(M::Multivector, N::Blade)" begin
+    @test_skip 1
+end
+
+@testset "!=(M::Multivector, N::Pseudoscalar)" begin
+    @test_skip 1
+end
+
+@testset "!=(M::Multivector, N::Scalar)" begin
+    @test_skip 1
+end
+
+@testset "!=(M::Multivector, N::One)" begin
+    @test_skip 1
+end
+
+@testset "!=(M::Multivector, N::Zero)" begin
+    @test_skip 1
+end
+
+@testset "!=(M::Multivector, N::Real)" begin
+    @test_skip 1
+end
+
+@testset "!=(M::Multivector, N::Vector)" begin
+    @test_skip 1
+end
+
+# ------ B::Blade
+
+@testset "!=(B::Blade, M::Multivector)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Blade, C::Pseudoscalar)" begin
     vectors = [3 3; 4 4; 0 1]
     test_dim = size(vectors, 1)
-    test_value = rand()
-    test_value = rand() > 0.5 ? test_value : -test_value
-
-    # --- B::AbstractBlade, C::AbstractBlade
+    test_value = 5
 
     for precision_type1 in subtypes(AbstractFloat)
         for precision_type2 in subtypes(AbstractFloat)
-            # B::Blade, C::Scalar
-            # B::Scalar, C::Blade
+            B = Blade{precision_type1}(vectors)
+            C = Pseudoscalar{precision_type2}(test_dim, test_value)
+            @test B != C
+        end
+    end
+end
+
+@testset "!=(B::Blade, C::Scalar)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
+
+    for precision_type1 in subtypes(AbstractFloat)
+        for precision_type2 in subtypes(AbstractFloat)
             B = Blade{precision_type1}(vectors)
             C = Scalar{precision_type2}(test_value)
             @test B != C
-            @test C != B
+        end
+    end
+end
 
-            # B::Blade, C::Pseudoscalar
-            # B::Pseudoscalar, C::Blade
-            B = Blade{precision_type1}(vectors)
-            C = Pseudoscalar{precision_type2}(test_dim, test_value)
+@testset "!=(B::Blade, C::One)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Blade, C::Zero)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Blade, C::Real)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
+
+    for precision_type in subtypes(AbstractFloat)
+        B = Blade{precision_type}(vectors)
+        C = test_value
+        @test B != C
+    end
+end
+
+@testset "!=(B::Blade, C::Vector)" begin
+    @test_skip 1
+end
+
+# ------ B::Pseudoscalar
+
+@testset "!=(B::Pseudoscalar, M::Multivector)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Pseudoscalar, C::Blade)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
+
+    for precision_type1 in subtypes(AbstractFloat)
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Pseudoscalar{precision_type2}(test_dim, test_value)
+            C = Blade{precision_type1}(vectors)
             @test B != C
-            @test C != B
+        end
+    end
+end
 
-            # B::Scalar, C::Pseudoscalar
-            # B::Pseudoscalar, C::Scalar
+@testset "!=(B::Pseudoscalar, C::Scalar)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
+
+    for precision_type1 in subtypes(AbstractFloat)
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Pseudoscalar{precision_type2}(test_dim, test_value)
+            C = Scalar{precision_type1}(test_value)
+            @test B != C
+        end
+    end
+end
+
+@testset "!=(B::Pseudoscalar, C::One)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Pseudoscalar, C::Zero)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Pseudoscalar, C::Real)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
+
+    for precision_type in subtypes(AbstractFloat)
+        B = Pseudoscalar{precision_type}(test_dim, test_value)
+        C = test_value
+        @test B != C
+    end
+end
+
+@testset "!=(B::Pseudoscalar, C::Vector)" begin
+    @test_skip 1
+end
+
+# ------ B::Scalar
+
+@testset "!=(B::Scalar, M::Multivector)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Scalar, C::Blade)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
+
+    for precision_type1 in subtypes(AbstractFloat)
+        for precision_type2 in subtypes(AbstractFloat)
+            B = Scalar{precision_type2}(test_value)
+            C = Blade{precision_type1}(vectors)
+            @test B != C
+        end
+    end
+end
+
+@testset "!=(B::Scalar, C::Pseudoscalar)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
+
+    for precision_type1 in subtypes(AbstractFloat)
+        for precision_type2 in subtypes(AbstractFloat)
             B = Scalar{precision_type1}(test_value)
             C = Pseudoscalar{precision_type2}(test_dim, test_value)
             @test B != C
-            @test C != B
         end
     end
+end
 
-    # --- B::Union{Blade, Pseudoscalar}, C::Real
-    #     B::Real, C::Union{Blade, Pseudoscalar}
+@testset "!=(B::Scalar, C::One)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Scalar, C::Zero)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Scalar, C::Real)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Scalar, C::Vector)" begin
+    @test_skip 1
+end
+
+# ------ B::One
+
+@testset "!=(B::One, M::Multivector)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::One, C::Blade)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::One, C::Pseudoscalar)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::One, C::Scalar)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::One, C::Zero)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::One, C::Real)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::One, C::Vector)" begin
+    @test_skip 1
+end
+
+# ------ B::Zero
+
+@testset "!=(B::Zero, M::Multivector)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Zero, C::Blade)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Zero, C::Pseudoscalar)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Zero, C::Scalar)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Zero, C::One)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Zero, C::Real)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Zero, C::Vector)" begin
+    @test_skip 1
+end
+
+# ------ B::Real
+
+@testset "!=(B::Real, M::Multivector)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Real, C::Blade)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
 
     for precision_type in subtypes(AbstractFloat)
-        # B::Blade, C::Real
-        # B::Real, C::Blade
-        B = Blade{precision_type}(vectors)
-        @test B != test_value
-        @test test_value != B
-
-        # B::Pseudoscalar, C::Real
-        # B::Real, C::Pseudoscalar
-        B = Pseudoscalar{precision_type}(test_dim, test_value)
-        @test B != test_value
-        @test test_value != B
+        B = test_value
+        C = Blade{precision_type}(vectors)
+        @test B != C
     end
+end
+
+@testset "!=(B::Real, C::Pseudoscalar)" begin
+    vectors = [3 3; 4 4; 0 1]
+    test_dim = size(vectors, 1)
+    test_value = 5
+
+    for precision_type in subtypes(AbstractFloat)
+        B = test_value
+        C = Pseudoscalar{precision_type}(test_dim, test_value)
+        @test B != C
+    end
+end
+
+@testset "!=(B::Real, C::Scalar)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Real, C::One)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Real, C::Zero)" begin
+    @test_skip 1
+end
+
+# ------ B::Vector
+
+@testset "!=(B::Vector, M::Multivector)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Vector, C::Blade)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Vector, C::Pseudoscalar)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Vector, C::Scalar)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Vector, C::One)" begin
+    @test_skip 1
+end
+
+@testset "!=(B::Vector, C::Zero)" begin
+    @test_skip 1
 end
