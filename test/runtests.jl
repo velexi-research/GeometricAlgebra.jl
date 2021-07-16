c22f0e6df5108ac02acc75117b9c38403b388dd9
Kevin Chu <kevin@velexi.com>
2021-07-16 09:44:48 -0700

    Remove unnecessary ExtendedTestSet from runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,7 +21,7 @@ using GeometricAlgebra
 
 # --- Test sets
 
-@testset ExtendedTestSet "All the tests" begin
+@testset "All the tests" begin
     @testset "Doctests" begin
         doctest(GeometricAlgebra)
     end
