34540c8f01ef3c583c6b3dc6eab0f3fdee720118
Documenter.jl <documenter@juliadocs.github.io>
2022-03-22 01:36:46 +0000

    build based on 89c4b52

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"docs-index/#Index","page":"Index","title":"Index","text":"","category":"section"},{"location":"docs-index/","page":"Index","title":"Index","text":"","category":"page"},{"location":"#GeometricAlgebra.jl","page":"Home","title":"GeometricAlgebra.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GeometricAlgebra.jl","category":"page"}]
+}
